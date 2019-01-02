a78ef846225d7340bafa71b20e583bf961bae41b
Maheen Heera Syed <maheen.heera-syed@zenika.com>
2019-01-02 20:17:04 +0800

    updating container name

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ docker push asia.gcr.io/${GOOGLE_PROJECT_ID}/${SERVER_APP}:${CIRCLE_SHA1}
 kubectl apply -f k8s
 
 kubectl set image deployments/client-deployment client=asia.gcr.io/${GOOGLE_PROJECT_ID}/${CLIENT_APP}:${CIRCLE_SHA1}
-kubectl set image deployments/server-deployment client=asia.gcr.io/${GOOGLE_PROJECT_ID}/${SERVER_APP}:${CIRCLE_SHA1}
+kubectl set image deployments/server-deployment server=asia.gcr.io/${GOOGLE_PROJECT_ID}/${SERVER_APP}:${CIRCLE_SHA1}
 
 
